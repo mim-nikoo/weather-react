a1b4071b1560173f19d89d77782562dd7a3894e1
mim-nikoo <134829937+mim-nikoo@users.noreply.github.com>
2023-07-27 04:50:05 -0700

    Added api integration

--- src/Weather.js
+++ src/Weather.js
@@ -1,84 +1,104 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
-  let weatherData = {
-    city: "Madrid",
-    temperature: 16,
-    description: "cloudy",
-    humidity: 80,
-    wind: 10,
-  };
-  return (
-    <div>
-      <div className="Weather">
-        <form className="mb-3">
-          <div className="row">
-            <div className="col-9">
-              <input
-                type="search"
-                placeholder="Type a city..."
-                className="form-control"
-                autocomplete="off"
-                autoFocus="on"
-              />
-            </div>
-            <div className="col-3">
-              <input
-                type="submit"
-                value="search"
-                className="btn btn-primary w-100"
-              />
-            </div>
-          </div>
-        </form>
-        <div className="overview">
-          <h1>{weatherData.city}</h1>
-          <ul>
-            <li>
-              last updated:<span></span>
-            </li>
-            <li>{weatherData.description}</li>
-          </ul>
-        </div>
-        <div className="row">
-          <div className="col-6">
-            <div className="d-flex weather-temperature">
-              <img src="http://openweathermap.org/img/w/01d.png" alt="Clear" />
-              <div>
-                <strong>{weatherData.temperature}</strong>
-                <span className="units">
-                  <a href="/" className="active">
-                    °C
-                  </a>{" "}
-                  |<a href="/">°F</a>
-                </span>
+export default function Weather(props) {
+  const [ready, setReady] = useState(false);
+  const [weatherData, setWeatherData] = useState({});
+  function handleResponse(response) {
+    console.log(response.data);
+    setReady(true);
+    setWeatherData({
+      temperature: response.data.temperature.current,
+      city: response.data.city,
+      description: response.data.condition.description,
+      humidity: response.data.temperature.humidity,
+      wind: response.data.wind.speed,
+    });
+  }
+
+  if (ready) {
+    return (
+      <div>
+        <div className="Weather">
+          <form className="mb-3">
+            <div className="row">
+              <div className="col-9">
+                <input
+                  type="search"
+                  placeholder="Type a city..."
+                  className="form-control"
+                  autoComplete="off"
+                  autoFocus="on"
+                />
+              </div>
+              <div className="col-3">
+                <input
+                  type="submit"
+                  value="search"
+                  className="btn btn-primary w-100"
+                />
               </div>
             </div>
-          </div>
-          <div className="col-6">
+          </form>
+          <div className="overview">
+            <h1>{weatherData.city}</h1>
             <ul>
               <li>
-                Humidity: <span>{weatherData.humidity}</span>%
+                last updated:<span></span>
               </li>
-              <li>
-                Wind: <span>{weatherData.wind}</span> km/h
-              </li>
+              <li>{weatherData.description}</li>
             </ul>
           </div>
+          <div className="row">
+            <div className="col-6">
+              <div className="d-flex weather-temperature">
+                <img
+                  src="http://openweathermap.org/img/w/01d.png"
+                  alt="Clear"
+                />
+                <div>
+                  <strong>{Math.round(weatherData.temperature)}</strong>
+                  <span className="units">
+                    <a href="/" className="active">
+                      °C
+                    </a>{" "}
+                    |<a href="/">°F</a>
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className="col-6">
+              <ul>
+                <li>
+                  Humidity: <span>{weatherData.humidity}</span>%
+                </li>
+                <li>
+                  Wind: <span>{Math.round(weatherData.wind)}</span> km/h
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="weather-forecast"></div>
         </div>
-        <div className="weather-forecast"></div>
+        <small>
+          <a
+            href="https://github.com/mim-nikoo/weather-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code
+          </a>
+          by Maryam Nikoomanesh
+        </small>
       </div>
-      <small>
-        <a
-          href="https://github.com/mim-nikoo/weather-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-source code
-        </a>
-        by Maryam Nikoomanesh
-      </small>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "b7fb433c7fddfa4844t24380290bd0do";
+    let units = "metric";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=${units}`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading..";
+  }
 }
